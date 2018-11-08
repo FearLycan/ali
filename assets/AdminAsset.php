94337f29ad43e62273b537376ff68ec51452352b
Damian Brończyk <damian.bronczyk@tauceti.email>
2018-11-08 15:41:08 +0100

    add admin panel style

--- assets/AdminAsset.php
+++ assets/AdminAsset.php
@@ -1,29 +1,24 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
 
 namespace app\assets;
 
 use yii\web\AssetBundle;
 
 /**
- * Main application asset bundle.
+ * Admin application asset bundle.
  *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
  */
 class AdminAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/admin/sb-admin.css',
         'css/font-awesome.min.css',
-        'css/admin.css',
+        'css/admin/style.css',
     ];
     public $js = [
+        'js/admin/bootstrap.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- modules/admin/views/layouts/admin.php
+++ modules/admin/views/layouts/admin.php
@@ -13,6 +13,14 @@ use yii\helpers\Url;
 use yii\widgets\Breadcrumbs;
 
 AdminAsset::register($this);
+
+$nails = [
+    'category', 'tag', 'item'
+];
+
+$system = [
+    'visitor'
+];
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -27,49 +35,124 @@ AdminAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
+<div class="wrap" id="wrapper">
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name . ' <small>Admin</small>',
-        'brandUrl' => Url::to(['default/index']),
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Admin', 'url' => ['index']],
-            Yii::$app->user->isGuest ? (
-            ['label' => 'Login', 'url' => ['/auth/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/auth/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->name . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
-    NavBar::end();
-    ?>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?= Url::to(['aadmin']) ?>"><?= Yii::$app->name ?> Admin Panel</a>
+        </div>
+        <!-- Top Menu Items -->
+        <ul class="nav navbar-right top-nav">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> <b
+                            class="caret"></b></a>
+                <ul class="dropdown-menu message-dropdown">
+                    <li class="message-preview">
+                        <a href="#">
+                            <div class="media">
+                                    <span class="pull-left">
+                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
+                                    </span>
+                                <div class="media-body">
+                                    <h5 class="media-heading"><strong>John Smith</strong>
+                                    </h5>
+                                    <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur...</p>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="message-footer">
+                        <a href="#">Read All New Messages</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bell"></i> <b
+                            class="caret"></b></a>
+                <ul class="dropdown-menu alert-dropdown">
+                    <li>
+                        <a href="#">Alert Name <span class="label label-default">Alert Badge</span></a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="#">View All</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>
+                    <?= Yii::$app->user->identity->name ?>
+                    <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="<?= Url::to(['/auth/logout']) ?>" data-method="POST"><i
+                                    class="fa fa-fw fa-power-off"></i> Log Out</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav side-nav">
+                <li>
+                    <a href="<?= Url::to(['/']) ?>" target="_blank"><i class="fa fa-home"></i> Strona główna</a>
+                </li>
+                <li class="<?= Yii::$app->controller->id == 'default' ? 'active' : 'no' ?>">
+                    <a href="<?= Url::to(['/admin']) ?>"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </nav>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'homeLink' => [
-                'label' => 'Admin',
-                'url' => Url::to(['default/index'])
-            ],
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]);
+    <div id="page-wrapper" style="min-height: 940px;">
 
-        ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        <?= $this->title ?>
+                        <small>Statistics Overview</small>
+                    </h1>
+
+                    <?= Breadcrumbs::widget([
+                        'homeLink' => [
+                            'label' => '<i class="fa fa-dashboard"></i>' . Html::encode(Yii::t('yii', ' Dashboard')),
+                            'url' => Url::to(['/admin']),
+                            'encode' => false
+                        ],
+                        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                    ])
+                    ?>
+                </div>
+                <div class="col-lg-12">
+                    <hr style="margin-top: -1px;">
+                </div>
+            </div>
+            <!-- /.row -->
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
     </div>
 </div>
 
