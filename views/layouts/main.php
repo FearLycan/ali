42bacc2fbd7e8922d076fd8825d3678032551c4d
Damian Brończyk <damian.bronczyk@tauceti.email>
2018-11-24 23:58:09 +0100

    add info about cookies

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -173,6 +173,24 @@ AppAsset::register($this);
 
 <?php $this->endBody() ?>
 <?= $this->blocks['script'] ?>
+<link rel="stylesheet" type="text/css"
+      href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css"/>
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+<script>
+    window.addEventListener("load", function () {
+        window.cookieconsent.initialise({
+            "palette": {
+                "popup": {
+                    "background": "#000"
+                },
+                "button": {
+                    "background": "#f1d600"
+                }
+            },
+            "theme": "classic"
+        })
+    });
+</script>
 </body>
 </html>
 <?php $this->endPage() ?>
