8e978dbf5cc7363630ecba58ae376ce753b7b51e
Damian Brończyk <damian.bronczyk@tauceti.email>
2018-11-25 00:10:29 +0100

    update

--- views/member/view.php
+++ views/member/view.php
@@ -31,7 +31,9 @@ $this->title = 'Member ' . $model->name;
             <div class="widget">
                 <ul class="list-group font-alt">
                     <li class="list-group-item">
-                        <a href="#">Go to <strong><?= Html::encode($model->name) ?></strong> Ali profile</a>
+                        <a href="<?= Url::to(['redirect/member', 'slug' => $model->member->slug]) ?>">
+                            Go to <strong><?= Html::encode($model->member->name) ?></strong> Ali profile
+                        </a>
                     </li>
                 </ul>
             </div>
