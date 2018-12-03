f72860193b9e5c5d26706392c6507594c648b51a
DamianBronczyk <damian.bronczyk@gmail.com>
2018-12-03 12:48:12 +0100

    update description

--- config/params.php
+++ config/params.php
@@ -2,7 +2,7 @@
 
 return [
     'admin-email' => 'mail@aligonewild.com',
-    'meta-description' => 'The best pics from the reviews of Aliexpress - Sexy Pics, Real Women, NSFW, Sensitive Content.',
+    'meta-description' => 'The biggest feedback aggregator from Aliexpress. All pics in one place - Intimates clothes, Bodysuits, Bra & Brief Sets, Real Women, NSFW, Sensitive Content.',
     'og_image' => ['property' => 'og:image', 'content' => 'https://aligonewild.com/images/site/og-image.png'],
     'og_type' => ['property' => 'og:type', 'content' => 'website'],
     'twitter_image' => ['property' => 'twitter:image', 'content' => 'https://aligonewild.com/images/site/og-image.png']
