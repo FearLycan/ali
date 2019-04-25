5ae41cbc7f06d2875d49f85f84da8dcccc0aa534
DamianBronczyk <damian.bronczyk@gmail.com>
2019-04-26 00:53:47 +0200

    update

--- views/product/_product.php
+++ views/product/_product.php
@@ -13,13 +13,14 @@ use yii\helpers\Url;
     <div class="shop-item-image">
         <img src="<?= $model->image ?>" alt="<?= $model->name ?>">
         <div class="shop-item-detail">
-            <a class="btn btn-round btn-b" href="<?= Url::to(['image/index', 'category' => $model->category->slug]) ?>">
+            <a class="btn btn-round btn-b" href="<?= Url::to(['image/index', 'category' => $model->category->slug]) ?>"
+               data-pjax="0">
                 <?= Html::encode($model->category->name) ?>
             </a>
         </div>
     </div>
     <h4 class="shop-item-title font-alt">
-        <a href="<?= Url::to(['product/view', 'id' => $model->ali_product_id]) ?>">
+        <a href="<?= Url::to(['product/view', 'id' => $model->ali_product_id]) ?>" data-pjax="0">
             <?= Helper::cutThis($model->name, 45) ?>
         </a>
     </h4>
