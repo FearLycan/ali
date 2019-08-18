9af4639ae8d3959b0a81977f7134f16133f47206
Damian Brończyk <damian.bronczyk@gmail.com>
2019-08-18 12:45:03 +0200

    update

--- commands/FunctionController.php
+++ commands/FunctionController.php
@@ -11,5 +11,6 @@ class FunctionController extends Controller
     public function actionSmartFixes()
     {
         Member::updateAll(['country_code' => 'UK'], ['=', 'country_code', 'GB']);
+        Member::updateAll(['country_code' => 'RS'], ['=', 'country_code', 'SRB']);
     }
 }
